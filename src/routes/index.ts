59c22fdf7231d5b2e7ab1955e93aa5baa4358cf5
jeremynoh <jeremynohile@gmail.com>
2022-07-13 01:32:21 +0200

    🥳 pkg works well + swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import auth from "../routes/auth";
 import file from "../routes/file";
 import dossiers from "../routes/dossiers";
 import dossier from "../routes/dossier";
 
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUI from "swagger-ui-express";
-const swaggerDocument = require("../docs/swagger.json");
+import path from "path";
 
 const routes = Router();
 
@@ -22,8 +20,15 @@ const routes = Router();
 //     : ["**/*.js"],
 // };
 
-// const swaggerDocs = swaggerJsDoc(swaggerOptions);
-routes.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+routes.get("/api-docs/swagger.json", (req, res) => {
+  let indexPath = path.join(__dirname, "/../swagger/swagger.json");
+  return res.sendFile(indexPath);
+});
+
+routes.get("/api-docs", (req, res) => {
+  let indexPath = path.join(__dirname, "/../swagger/index.html");
+  return res.sendFile(indexPath);
+});
 
 routes.use("/auth", auth);
 routes.use("/dossier", dossier);
